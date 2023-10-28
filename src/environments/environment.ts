a0e54b1037bfdb359e456e8343bf6ed45c6c15f5
unknown <emayeng4@miumg.edu.gt>
2023-10-28 11:02:06 -0600

    Login

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-login-e65c6',
+    appId: '1:1085989691076:web:1c1fd16cd341629d72f391',
+    storageBucket: 'angular-login-e65c6.appspot.com',
+    apiKey: 'AIzaSyCT1N5fgkGSupKt-jeziCMbt1oPqVx6rbo',
+    authDomain: 'angular-login-e65c6.firebaseapp.com',
+    messagingSenderId: '1085989691076',
+  },
+};
